bb47adce2e1a7cc4d4a952709dfb7cf1807379ef
TetraGamesTeam <mr.max.kh@gmail.com>
2024-10-27 13:26:22 +0700

    Немножко поправил

--- express-server/api-server.js
+++ express-server/api-server.js
@@ -41,14 +41,6 @@ fs.readFile("../config/dictionary.yml", "utf8", (err, data) => {
   }
 });
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
 function unixTimeToDateTime(timestamp) {
   const date = new Date(timestamp * 1000);
   const day = date.getDate().toString().padStart(2, "0");
@@ -65,7 +57,6 @@ function unixTimeToTime(timestamp) {
   const date = new Date(timestamp * 1000);
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear().toString().slice(-2);
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
